878f79aae4190ee472ec6ecc8af94326c4f8f843
Rodrigo Ballestero <ballesterorj@gmail.com>
2016-06-24 21:32:52 -0300

    Avances de grupo, version final

--- src/package_tp/Equipo.scala
+++ src/package_tp/Equipo.scala
@@ -41,40 +41,60 @@ case class Equipo(val nombreDeEquipo: String, val pozoComun: Int = 0, val heroes
       throw new TareaFallidaException(unaTarea, this)
     }
   }
+
   def realizarMision(unaMision: Mision): Equipo = {
-  
-      val equipoFinal = unaMision.tareas.foldLeft(this)((unEquipo: Equipo, unaTarea: Tarea) => unEquipo.realizarTarea(unaTarea))
-      unaMision.recompensa(equipoFinal)
-   
+    val equipoFinal = unaMision.tareas.foldLeft(this)((unEquipo: Equipo, unaTarea: Tarea) => unEquipo.realizarTarea(unaTarea))
+    unaMision.recompensa(equipoFinal)
   }
 
   type criterioMejorMision = (Equipo, Equipo) => Boolean
   def elegirMision(criterio: criterioMejorMision, mision1: Mision, mision2: Mision): Mision = {
-  val equipoConMision1:Try[Equipo]=Try(this.realizarMision(mision1))
-  val equipoConMision2:Try[Equipo]=Try(this.realizarMision(mision2))
-  val tupla=(equipoConMision1,equipoConMision2)
-  tupla match{
-    case (Failure(_),Success(rdo))=>mision2
-    case (Success(rdo),Failure(_))=>mision1
-    case (Success(rdo1),Success(rdo2))=>
-      {
-  if (criterio(rdo1, rdo2)) { mision1 }
-    else { mision2 }
-  
-      }
-    case(Failure(exception),Failure(_))=>throw exception
-    
-  }
-  }
-
-  def entrenar(misiones: List[Mision], criterio: criterioMejorMision): Equipo = {
-    misiones match {
-      case x :: xs => this.realizarMision(misiones.fold(x)((mis1, mis2) => elegirMision(criterio, mis1, mis2))).
-        entrenar(xs, criterio)
-      case x :: Nil => this.realizarMision(x)
-
+    val equipoConMision1: Try[Equipo] = Try(this.realizarMision(mision1))
+    val equipoConMision2: Try[Equipo] = Try(this.realizarMision(mision2))
+    val tupla = (equipoConMision1, equipoConMision2)
+    tupla match {
+      case (Failure(_), Success(rdo)) => mision2
+      case (Success(rdo), Failure(_)) => mision1
+      case (Success(rdo1), Success(rdo2)) =>
+        {
+          if (criterio(rdo1, rdo2)) { mision1 }
+          else { mision2 }
+        }
+      case (Failure(exception), Failure(_)) => throw exception
     }
   }
-}
+
+  def mejorMision(criterio: criterioMejorMision, mision1: Mision, mision2: Mision): Boolean = {
+
+    var rdo1, rdo2 = this
+
+    try {
+      rdo1 = this.realizarMision(mision1)
+    } catch {
+      case e: TareaFallidaException => return false
+    }
+
+    try {
+      rdo2 = this.realizarMision(mision2)
+    } catch {
+      case e: TareaFallidaException => return true
+    }
+
+    return criterio(rdo1, rdo2)
+  }
+
+  /*def entrenar(misiones: List[Mision], criterio: criterioMejorMision): Equipo = {
+    misiones match {
+      case x :: xs  => this.realizarMision(misiones.fold(x)((mis1, mis2) => elegirMision(criterio, mis1, mis2))).entrenar(xs, criterio)
+      case x :: Nil => this.realizarMision(x)
+      case _        => this
+    }
+  }*/ 
   
+  def entrenar(misiones: List[Mision], criterio: criterioMejorMision): Equipo = {
+   misiones.sortWith((mision1:Mision,mision2:Mision)=>mejorMision(criterio,mision1,mision2)).
+   foldLeft(this)((equipo:Equipo,mision:Mision)=>equipo.realizarMision(mision))
+  }
+ 
+}  
   
--- test/package_tp/Tests.scala
+++ test/package_tp/Tests.scala
@@ -5,36 +5,46 @@ import org.junit.Before
 import org.junit.Assert._
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.Intercepter
 
-class Tests  {
-  
-  var cascoVikingo:Item=_
-  var unHeroe:Heroe=_
-  var heroePt:Heroe=_
+class Tests {
+
+  var cascoVikingo: Item = _
+  var unHeroe: Heroe = _
+  var heroePt: Heroe = _
   @Before
-  def setup(){
+  def setup() {
+
+    cascoVikingo = new Casco(((x: Heroe) => x.fuerza > 30), 5,
+      ((heroe: Heroe) => heroe.copy(hp = heroe.hp + 10)))
+
+    unHeroe = new Heroe(10, 40, 30, 40)
+    heroePt = new Heroe(1, 1, 1, 1)
     
-   cascoVikingo =new Casco(((x:Heroe) => x.fuerza>30),5,
-          ((heroe:Heroe) => heroe.copy(hp=heroe.hp+10)))
     
-   unHeroe = new Heroe(10,40,30,40)
-   heroePt=new Heroe(1,1,1,1)
     
+
   }
+
   @Test
-  def `puede_equipar_un_heroe_casco_vikingo`= {
+  def `puede_equipar_un_heroe_casco_vikingo` = {
     assertTrue(cascoVikingo.puedeEquipar(unHeroe))
-  }  
-  
-  @Test
-  def `equipar_un_heroe_con_casco_vikingo`= {
-    var heroeEquipado = unHeroe.equipar(cascoVikingo)
-    
-    assertEquals(heroeEquipado.inventario.cabeza,Some(cascoVikingo))
-    assertEquals(heroeEquipado.getStat(Stat.hp),unHeroe.getStat(Stat.hp)+10)
   }
-  @Test val expected= classOf[NoEquipableException]
-  def `equipar_un_heroe_pt_con_casco_vikingo_y_no_se_equipa_por_no_cumplir`= {
-     var heroeEquipado = heroePt.equipar(cascoVikingo)   
-  } 
+
+  @Test
+  def `equipar_un_heroe_con_casco_vikingo` = {
+    var heroeEquipado = unHeroe.equipar(cascoVikingo)
+
+    assertEquals(heroeEquipado.inventario.cabeza, Some(cascoVikingo))
+    assertEquals(heroeEquipado.getStat(Stat.hp), unHeroe.getStat(Stat.hp) + 10)
+  }
+
+  @Test val expected = classOf[NoEquipableException]
+  def `equipar_un_heroe_pt_con_casco_vikingo_y_no_se_equipa_por_no_cumplir` = {
+    var heroeEquipado = heroePt.equipar(cascoVikingo)
+  }
   
+  
+  
+  
+  
+
 }
